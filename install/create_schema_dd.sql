1f2e2ab38cf4ff22d4d084accc84a54842901c14
Axel Rau <axel.rau@chaos1.de>
2016-06-18 21:46:47 +0000

    Finished SQL for DB schemata

--- install/create_schema_dd.sql
+++ install/create_schema_dd.sql
@@ -2,20 +2,43 @@
 SET log_min_messages='error';
 
 -- 'Attention!! drops referencing schema pki  Attention!!'
---DROP SCHEMA IF EXISTS dd CASCADE;
+DROP SCHEMA IF EXISTS dd CASCADE;
+
 
 CREATE SCHEMA dd;            -- 'data dictionary for project serverPKI
 
-CREATE DOMAIN created
+
+
+
+------------------------------ 'created' timestamp
+CREATE DOMAIN dd.created
     AS TIMESTAMP
     DEFAULT TIMESTAMP 'NOW'
     NOT NULL;
+ALTER DOMAIN dd.created OWNER TO pki_dev;
 
-CREATE DOMAIN updated
+------------------------------ 'updated' timestamp
+CREATE DOMAIN dd.updated
     AS TIMESTAMP
     DEFAULT TIMESTAMP 'NOW'
     NOT NULL;
+ALTER DOMAIN dd.updated OWNER TO pki_dev;
 
-CREATE TYPE subject_type AS ENUM ('client', 'server');
+------------------------------ TCP/UDP port number number
+CREATE DOMAIN dd.port_number
+    AS int4
+    CONSTRAINT port_number CHECK (VALUE >= 0 AND VALUE <= 65536);
+ALTER DOMAIN dd.port_number OWNER TO pki_dev;
+
+------------------------------ type of Subject
+CREATE TYPE dd.subject_type AS ENUM ('client', 'server');
+ALTER TYPE dd.subject_type OWNER TO pki_dev;
+
+------------------------------ type of Place
+CREATE TYPE dd.place_cert_file_type AS ENUM (
+    'separate', 'combined cert key', 'combined cacert cert key');
+ALTER TYPE dd.subject_type OWNER TO pki_dev;
+
+
 
 GRANT USAGE ON SCHEMA dd TO pki_dev;
--- install/create_schema_pki.sql
+++ install/create_schema_pki.sql
@@ -9,11 +9,10 @@ DROP SCHEMA IF EXISTS pki CASCADE;
 CREATE SCHEMA pki               -- DB schema for project serverPKI'
 
 
-CREATE TABLE Certificates (     -- All the details of a certificate
+CREATE TABLE Certificates (     -- The certificate class
   id                SERIAL          PRIMARY KEY,    -- 'PK of Certificates table'
-  subject           int4            NOT NULL UNIQUE,-- 'Subject of certificate'
-  created           dd.created                     ,   -- 'Date of record creation'
-  remarks           text                            -- 'Remarks'
+  created           dd.created,                     -- 'time of record update'
+  remarks           TEXT                            -- 'Remarks'
 )
 
 
@@ -21,26 +20,150 @@ CREATE TABLE Subjects (         -- A Subject or an alternate name of a certifica
   id                SERIAL          PRIMARY KEY,    -- 'PK of Subjects table'
   type              dd.subject_type NOT NULL        -- 'Type of subject'
                                         DEFAULT 'server', 
-  name              text            NOT NULL UNIQUE,-- 'Either FQDN or user name'
-  isAltname         boolean         NOT NULL
+  name              TEXT            NOT NULL UNIQUE,-- 'Either FQDN or user name'
+  isAltname         BOOLEAN         NOT NULL
                                         DEFAULT TRUE, 
-  certificate       int4                            -- 'certifcate for this subject'
+  certificate       INT4                            -- 'certifcate for this subject'
                         REFERENCES Certificates
                         ON DELETE SET NULL
                         ON UPDATE SET NULL,
-  created           dd.created                     ,   -- 'time of record creation'
-  pdated            dd.updated                     ,   -- 'time of record update'
-  remarks           text                            -- 'Remarks'
+  updated           dd.updated,                     -- 'time of record update'
+  created           dd.created,                     -- 'time of record creation'
+  remarks           TEXT                            -- 'Remarks'
 )
 
+
+CREATE TABLE Services (         -- Service and port combination for TLSA-RR
+  id                SERIAL          PRIMARY KEY,    -- 'PK of Services table'
+  name              TEXT            NOT NULL ,      -- 'Name of service
+  port              dd.port_number  NOT NULL ,      -- 'tcp / udp port number 
+  certificate       int4                            -- 'certificate'
+                        REFERENCES Certificates
+                        ON DELETE SET NULL
+                        ON UPDATE SET NULL,
+  TLSAprefix        TEXT            NOT NULL,
+  created           dd.created,                     -- 'time of record update'
+  updated           dd.updated,                     -- 'time of record update'
+  remarks           TEXT,                           -- 'Remarks'
+
+    UNIQUE (name, port)
+)
+
+
+CREATE TABLE Places (         -- Places hold filesystem and exec data on target
+  id                SERIAL          PRIMARY KEY,    -- 'PK of Places table'
+  name              TEXT            NOT NULL UNIQUE,-- 'Name of place
+  cert_file_type    dd.place_cert_file_type         -- 'which cert amd key files'
+                                    DEFAULT 'separate'
+                                    NOT NULL, 
+  cert_path         TEXT            NOT NULL,       -- 'path to cert dir' 
+  key_path          TEXT            NOT NULL,       -- 'path to key dir' 
+  uid               int2                    ,       -- 'uid for chown of key file'
+  gid               int2                    ,       -- 'gid for chown of key file'
+  mode              int2                    ,       -- 'use this mode instead of 0400 for key file'
+  chownBoth         BOOLEAN         NOT NULL        -- 'chown both cert and key file'
+                                    DEFAULT FALSE,
+  pgLink            BOOLEAN         NOT NULL        -- 'create link for pqlib'
+                                    DEFAULT FALSE,
+  reload_command    TEXT            ,               -- 'shell command to reload service'
+  created           dd.created,                     -- 'time of record update'
+  updated           dd.updated,                     -- 'time of record update'
+  remarks           TEXT                            -- 'Remarks'
+)
+
+
+CREATE TABLE DistHosts (         -- Hosts where targets located (cert and key files)
+  id                SERIAL          PRIMARY KEY,    -- 'PK of DistHosts table'
+  FQDN              TEXT            NOT NULL UNIQUE,-- 'FQDN of host'
+  jailroot          TEXT      ,                     -- 'path to root of jails'
+  updated           dd.updated,                     -- 'time of record update'
+  created           dd.created,                     -- 'time of record creation'
+  remarks           TEXT                            -- 'Remarks'
+)
+
+
+CREATE TABLE Jails (              -- FreeBSD jail, to place cert at
+  id                SERIAL          PRIMARY KEY,    -- 'PK of Jails table'
+  name              TEXT            NOT NULL UNIQUE,-- 'FQDN of host'
+  distHost          int4            NOT NULL        -- 'host, hosting this jail'
+                        REFERENCES DistHosts
+                        ON DELETE CASCADE
+                        ON UPDATE CASCADE,
+  updated           dd.updated,                     -- 'time of record update'
+  created           dd.created,                     -- 'time of record creation'
+  remarks           TEXT                            -- 'Remarks'
+)
+
+
+CREATE TABLE Targets (    -- Target describes where and how certs and keys are deployed
+  id                SERIAL          PRIMARY KEY,    -- 'PK of DistHosts table'
+  distHost          int4            NOT NULL        -- 'host, hosting this jail/cert'
+                        REFERENCES DistHosts
+                        ON DELETE CASCADE
+                        ON UPDATE CASCADE,
+  jail              int4            NOT NULL        -- 'jail, hosting this cert'
+                        REFERENCES Jails
+                        ON DELETE CASCADE
+                        ON UPDATE CASCADE,
+  place             int4            NOT NULL        -- 'cert placed here'
+                        REFERENCES Places
+                        ON DELETE CASCADE
+                        ON UPDATE CASCADE,
+  certificate       int4            NOT NULL        -- 'subject of target'
+                        REFERENCES Certificates
+                        ON DELETE CASCADE
+                        ON UPDATE CASCADE,
+  updated           dd.updated,                     -- 'time of record update'
+  created           dd.created,                     -- 'time of record creation'
+  remarks           TEXT                            -- 'Remarks'
+)
+
+
+CREATE TABLE CertInstance (        -- certificate instances being issued
+  id                SERIAL          PRIMARY KEY,    -- 'PK of CertInstance table'
+  certificate       int4            NOT NULL        -- 'Certificate Class'
+                        REFERENCES Certificates
+                        ON DELETE RESTRICT
+                        ON UPDATE RESTRICT,
+  state             TEXT            NOT NULL,       -- 'state tbd'
+  cert              TEXT            NOT NULL,       -- 'PEM encoded certificate'
+  key               TEXT            NOT NULL,       -- 'PEM encoded key'
+  cert_key          TEXT            NOT NULL,       -- 'PEM encoded cert+key'
+  CAcert_cert_key   TEXT            NOT NULL,       -- 'PEM encoded cert+key+CAcert'
+  TLSA              TEXT            NOT NULL,       -- 'hex ascii encoded TLSA hash'
+  issued            dd.created,                     -- 'time of record update'
+  expires           dd.created,                     -- 'time of record creation'
+  updated           dd.updated,                     -- 'time of record update'
+  remarks           TEXT                            -- 'Remarks'
+)
 ;                       -- CREATE SCHEMA pki -----------------------------------
 
 
-ALTER TABLE Certificates                            -- 'forward referenced FOREIGN KEYS'
-    ADD FOREIGN KEY ( subject ) REFERENCES Subjects
-                        ON DELETE CASCADE          -- 'We will be deleted with the subject'
-                        ON UPDATE RESTRICT
-                                DEFERRABLE ;
+                        -- TRIGGERS --------------------------------------------
+
+
+CREATE OR REPLACE FUNCTION Ensure_exactly_one_none_altname_subject_per_cert() RETURNS TRIGGER AS $$
+    BEGIN
+        IF NEW.isAltname THEN
+            RETURN NEW;
+        END IF;
+        IF (SELECT COUNT(*)
+                FROM Subjects S
+                WHERE S.certificate = NEW.certificate AND NOT S.isAltname ) > 0 THEN
+            RAISE EXCEPTION '?Only one none-alternate-name-Subject per Certificate allowed';  
+        ELSE
+            RETURN NEW;
+        END IF;
+    END;
+$$ LANGUAGE 'plpgsql';
+
+GRANT EXECUTE ON FUNCTION Ensure_exactly_one_none_altname_subject_per_cert() TO pki_dev;
+DROP TRIGGER IF EXISTS Ensure_exactly_one_none_altname_subject_per_cert ON Subjects;
+CREATE TRIGGER Ensure_exactly_one_none_altname_subject_per_cert BEFORE INSERT OR UPDATE
+    ON Subjects FOR EACH ROW 
+    EXECUTE PROCEDURE Ensure_exactly_one_none_altname_subject_per_cert();
+
+
 
 
 GRANT USAGE ON SCHEMA pki TO pki_dev;
