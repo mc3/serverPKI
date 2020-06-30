c5a4dac5e6a7edcab102ef43f6c9ede08374e9bc
Axel Rau <axel.rau@chaos1.de>
2020-06-30 17:41:43 +0200

    Merged CKD into master

--- serverPKI/operate.py
+++ serverPKI/operate.py
@@ -16,64 +16,75 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with serverPKI.  If not, see <http://www.gnu.org/licenses/>.
 """
-
 # commandline interface module
 
 import sys
+from typing import List, Dict
+from subprocess import Popen, PIPE
+
 from paramiko import util
+from postgresql import driver as db_conn
 
-from serverPKI.certdist import deployCerts, consolidate_TLSA, consolidate_cert, delete_TLSA, export_instance
-from serverPKI.config import LE_SERVER, LE_EMAIL, Pathes
-from pathlib import Path
-
-from serverPKI.utils import options as opts
-
-from serverPKI.utils import get_name_string, options_set, check_actions
-from serverPKI.utils import updateSOAofUpdatedZones
-from serverPKI.utils import names_of_local_certs_to_be_renewed, print_certs
-
-from serverPKI.utils import options_set, check_actions, updateSOAofUpdatedZones
-from serverPKI.utils import read_db_encryption_key,encrypt_all_keys,decrypt_all_keys
-
-from serverPKI.db import DbConnection as dbc
-from serverPKI.utils import sld, sli, sln, sle
 from serverPKI.cacert import issue_local_CAcert
-from serverPKI.cert import Certificate
+from serverPKI.cert import Certificate, CertType, CM
+from serverPKI.certdist import deployCerts, consolidate_TLSA, consolidate_cert, delete_TLSA, export_instance
+from serverPKI.db import DbConnection as dbc
+from serverPKI.issue_LE import issue_LE_cert
+from serverPKI.issue_local import issue_local_cert
+
+from serverPKI.utils import parse_options, parse_config, get_config
+
+from serverPKI.utils import get_name_string, get_version_string, options_set, check_actions
+from serverPKI.utils import names_of_local_certs_to_be_renewed, print_certs
+from serverPKI.utils import options_set, check_actions, updateSOAofUpdatedZones
+from serverPKI.utils import read_db_encryption_key, encrypt_all_keys, decrypt_all_keys
+from serverPKI.utils import sld, sli, sln, sle
 from serverPKI.schedule import scheduleCerts
-            
+
 from automatoes.register import register
+
 
 def execute_from_command_line():
 
+    global options
 
-    all_cert_names = []
-    our_cert_names = []
-    our_certs = {}
-    
-    
-    util.log_to_file('sftp.log')
-    
-    
-    sli('operateCA [{}]started with options {}'.format(
-                        get_name_string(), options_set()))
+    """
+    if hostname('hermes'):
+        import pydevd_pycharm
+        pydevd_pycharm.settrace('axels-imac.in.chaos1.de', port=4711, stdoutToServer=True, stderrToServer=True)
+    """
+
+    all_cert_names: List[str, ...] = []
+    our_cert_names: List[str, ...] = []
+    our_certs: Dict[str, Certificate] = {}
+
+    ##util.log_to_file('sftp.log')
+
+    options = opts = parse_options()        # globals not re-initialized
+    parse_config()                          # globals not re-initialized
+    (DBAccount, Misc, Pathes, X509atts) = get_config()
+    db_name = DBAccount.dbDatabase
+
+    sli('operateCA [{}-{}] started with options {}'.format(
+        db_name, get_version_string(), options_set()))
     check_actions()
-     
+
     pe = dbc('serverpki')
-    db = pe.open()
-    
+    db: db_conn = pe.open()
+
     read_db_encryption_key(db)
-    
-    row_list = db.query("""
-        SELECT name from Subjects
-            WHERE isAltName = FALSE
-            ORDER BY name""",)
-    
-    
-    for (name,) in row_list:
-        all_cert_names.append(name)
-    
-    sli('{} certificates in configuration'.format(len(all_cert_names)))
-    
+
+    all_cert_names = Certificate.names(db)
+    all_disthost_names = Certificate.disthost_names(db)
+    # preload CMs, CIs and CKSs of our CAs
+    cas = []
+    for name in (Misc.SUBJECT_LOCAL_CA, Misc.SUBJECT_LE_CA):
+        if name in all_cert_names:      # does CA exist in DB?
+            cm = CM(db, name)
+            cas.append(name)
+
+    sli('{} certificates and CAs {} in DB'.format(len(all_cert_names), cas))
+
     if opts.encrypt:
         if encrypt_all_keys(db):
             sys.exit(0)
@@ -86,7 +97,6 @@ def execute_from_command_line():
         if issue_local_CAcert(db):
             sys.exit(0)
         sys.exit(1)
-            
 
     if opts.all:
         our_cert_names = all_cert_names
@@ -96,11 +106,11 @@ def execute_from_command_line():
                                                             opts.distribute)
         if not opts.create:
             opts.create = not opts.distribute
-    
-    cert_name_set = set(our_cert_names)
-    
+
+    cert_name_set: set = set(our_cert_names)
+
     error = False
-    
+
     if opts.only_cert:
         error = False
         for i in opts.only_cert:
@@ -109,7 +119,7 @@ def execute_from_command_line():
                 error = True
         if not error:
             cert_name_set = set(opts.only_cert)
-    
+
     else:
         if opts.cert_to_be_included:
             error = False
@@ -119,7 +129,7 @@ def execute_from_command_line():
                     error = True
             if not error:
                 cert_name_set = set(opts.cert_to_be_included)
-        
+
         if opts.cert_to_be_excluded:
             error = False
             for i in opts.cert_to_be_excluded:
@@ -127,61 +137,115 @@ def execute_from_command_line():
                     sle("{} not in configuration. Can't be excluded".format(i))
                     error = True
             if not error:
-                cert_name_set -= set(cert_to_be_excluded)
-    
+                cert_name_set -= set(opts.cert_to_be_excluded)
+    if opts.only_host:
+        for i in opts.only_host:
+            if i not in all_disthost_names:
+                sle("{} not in configuration. Can't use in --limit-to-disthost".format(i))
+                error = True
+    if opts.skip_host:
+        for i in opts.skip_host:
+            if i not in all_disthost_names:
+                sle("{} not in configuration. Can't use in --skip-disthost".format(i))
+                error = True
+
     if error:
         sle('Stopped due to command line errors')
         sys.exit(1)
-    
+
     our_cert_names = sorted(list(cert_name_set))
-    
+
     for name in our_cert_names:
-        c = Certificate(db, name)
-        if c: our_certs[name] = c
-    
+        cm = CM(db, name)
+        if cm.in_db: our_certs[name] = cm
+
     if opts.check_only and not opts.schedule:
         sli('No syntax errors found in configuration.')
         ##sli('Selected certificates:\n\r{}'.format(our_cert_names))
         print_certs(db, our_cert_names)
         sys.exit(0)
-    
+
     sld('Selected certificates:\n\r{}'.format(our_cert_names))
-    
+
     if opts.schedule:
         sli('Scheduling actions.')
-        scheduleCerts(db, our_cert_names)
+        scheduleCerts(db, our_certs)
     else:
         if opts.create:
             sli('Creating certificates.')
             for c in our_certs.values():
-                if c.create_instance():
-                    continue
+                if c.cert_type == CertType('LE'):
+                    if issue_LE_cert(c):
+                        continue
+                elif  c.cert_type == CertType('local'):
+                    if issue_local_cert(c):
+                        continue
+                else:
+                    raise AssertionError('Invalid CertType in {}'.format(c.name))
                 sle('Stopped due to error')
                 sys.exit(1)
+
         if opts.distribute:
             sli('Distributing certificates.')
             deployCerts(our_certs)
-    
+
     if opts.sync_disk:
         for c in our_certs.values():
             consolidate_cert(c)
-    
+
     if opts.sync_tlsas:
         for c in our_certs.values():
             consolidate_TLSA(c)
         updateSOAofUpdatedZones()
-    
+
     if opts.remove_tlsas:
         for c in our_certs.values():
             delete_TLSA(c)
         updateSOAofUpdatedZones()
-    
+
     if opts.cert_serial:
         sli('Exporting certificate instance.')
         export_instance(db)
-    
+
     if opts.register:
         sli('Registering a new Let\'s Encrypt Account.\n With URI:{}\n'
-                ' and e-mail {}'.format(LE_SERVER, LE_EMAIL))
-        register(LE_SERVER, Pathes.le_account, LE_EMAIL, None)
-    
+            ' and e-mail {}'.format(Misc.LE_SERVER, Misc.LE_EMAIL))
+        register(Misc.LE_SERVER, Pathes.le_account, Misc.LE_EMAIL, None)
+
+
+def issue(db: db_conn, cert_meta: Certificate) -> bool:
+    """
+    Issue a new certificate
+    :param db: opened database connection
+    :param cert_meta: cert meta data instance describing attributes of new certs
+    :return: True if success, False otherwise
+    :exceptions: May raise assertion error on corrupt DB contents
+    """
+    """
+    Issue a new certificate instance and store it
+    in the DB table certinstances.
+
+    @rtype:             bool, true if success
+    @exceptions:        AssertionError
+    """
+    with db.xact(isolation='SERIALIZABLE', mode='READ WRITE'):
+        if cert_meta.cert_type == CertType('LE'):
+            result = issue_LE_cert(cert_meta)
+        elif cert_meta.cert_type == CertType('local'):
+            result = issue_local_cert(cert_meta)
+        else:
+            raise AssertionError
+    if result:
+        return True
+    return False
+
+def hostname(name:str) -> bool:
+    """
+    Check for hostname of development server
+    No PyCharm debugger if running test suite on localhost
+    :param name: hostname of remote development server
+    :return: True if hostname match name
+    """
+    p = process=Popen('hostname', stdout=PIPE, text=True)
+    stdout, stderr = p.communicate()
+    return stdout.strip() == name
